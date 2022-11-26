1c502b44f8ccd65d557b8948d29d73e53ab7fb64
Nikita <nik.bagnuk@mail.ru>
2022-11-26 10:20:11 +0300

    Added gitignore

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+fun main(){
+    println("Hello, it's calculator")
+    while(true){
+        println("Enter the first number")
+        val first = readLine()?.toDoubleOrNull()
+        println("Enter one more number")
+        val second = readLine()?.toDoubleOrNull()
+        println("Choose the operator(+,-,*,/)")
+        val operator = readLine()
+        if (first == null || second == null || operator.isNullOrEmpty()){
+            println("You entered incorrect data")
+        }else{
+            val result = calculator(first,second,operator)
+            println("Result is $result")
+        }
+    }
+
+
+
+
+}
+
+fun calculator(fir: Double, sec: Double, op: String): Double {
+    var res = 0.0
+    when(op){
+        "+" -> res = fir + sec
+        "-" -> res = fir - sec
+        "*" -> res = fir * sec
+        "/" -> res = fir / sec
+    }
+    return res;
+
+}
